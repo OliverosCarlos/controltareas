a520c6c985f00f424945164c0b2cc41e92158651
dianaRamosAg <dilaramosag@ittepic.edu.mx>
2019-12-09 15:59:18 -0600

    acomodo

--- src/app/usuarios/registro/registro.component.ts
+++ src/app/usuarios/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/Usuario';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../../servicios/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
-
+  usuario: Usuario  = {
+    nombre_usuario    :'',
+    edad :'',
+    ocupacion      :'',
+    correo       :'',
+    contrasena_usuario   :'',
+    ruta_img_usuario:''
+  };
+  constructor(private usuarioServicio: UsuarioService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  selectedFile: File;
+  imagePreview: string | ArrayBuffer;
+  edit: Boolean = false;
   ngOnInit() {
+  }
+  
+  onFileUpload(event){
+    console.log(event.target.files);
+    this.selectedFile = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+    this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(this.selectedFile);
   }
 
 }
