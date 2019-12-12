70929c782dd9c09fef785f4bd5a6cc154f4454d4
OliverosCarlos <44650476+OliverosCarlos@users.noreply.github.com>
2019-12-12 10:52:31 -0700

    ver horario

--- src/app/horario/mostrar-horario/mostrar-horario.component.ts
+++ src/app/horario/mostrar-horario/mostrar-horario.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mostrar-horario.component.css']
 })
 export class MostrarHorarioComponent implements OnInit {
+  horas = []; 
+  
 
   constructor() { }
 
   ngOnInit() {
+    for(let i = 0;i<=24;i++){
+      this.horas[i] = i;
+    }
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
   }
+
+
+
 
 }
